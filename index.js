4b3002b3fd48169551f5e18ab3005c0de3e53ee2
trizzle <robinson.tory.e@gmail.com>
2022-10-06 15:43:12 -0400

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const port = 8080;
 
-app.listen(port, () => {
-  console.log(`Server listening at port:${port}`);
-});
+app.listen(process.env.PORT || port, () => {
+  console.log(`Listening for server on Port: ${port}`)
+})
